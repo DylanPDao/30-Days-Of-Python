50e91df0b0f637198588bc010560b757bcb81a07
Dylan Dao <dylanpdao@gmail.com>
2024-09-11 01:37:40 +0000

    finished day 2

---
+++ 02_Day_Variables_builtin_functions/variables_exercise.py
@@ -0,0 +1,17 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Dylan'
+last_name = 'Dao'
+full_name = 'Dylan Dao'
+country = 'USA'
+city = 'cali-city'
+age = '199'
+year = '2024'
+is_married = False
+is_true = True
+is_light_on = True
+is_taken, would_want_to_be_taken, who_do_you_want_to_meet = False, False, 'Jaimie'
+
+print('Type of First Name:', type(first_name))
+print('Length of last name:', len(last_name))
+print(len(first_name) == len(last_name))
