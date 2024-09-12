e2b42b0b93441db6bf82d35f50822e728400cef1
Dylan Dao <dylanpdao@gmail.com>
2024-09-12 02:50:34 +0000

    finished day 4

---
+++ 04_Day_Strings/strings.py
@@ -0,0 +1,9 @@
+concat1 = "thirty" + "days" + "of" + "python"
+company = "coding For All"
+print(company)
+print(len(company))
+print(company.upper)
+print(company.lower)
+first_word = company[0:6]
+print(first_word)
+print(company.replace('coding', 'python'))
